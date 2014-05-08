e7f3fe5713a744ddc7585ecd11992699bfac7efb
DoctorWhoof <DoctorWhoof@users.noreply.github.com>
2014-05-07 23:50:18 -0700

    Cleaned up comments.

--- pimotion.py
+++ pimotion.py
@@ -10,7 +10,6 @@ import os
 import sys
 import time
 import subprocess
-import errno
 
 from datetime import datetime
 from PIL import Image
@@ -18,43 +17,43 @@ from PIL import Image
 class Motion:
 
 	def __init__( self ):
-												# For best results (by far) set width and height to exactly half camera.resolution. Anything else may result in cropping and noisy images.
-		self.width = 960						# Video file horizontal resolution
-		self.height = 720						# Video file vertical resolution
-		self.framerate = 15						# Video file framerate.
-		self.minimumLength = 10					# Minimum duration of a recording after motion stops. Lower number results in more files, higher number results in fewer, longer files.
-		self.rotation = 0						# Rotates image (warning: cropping will occur!)
+								# For best results (by far) set width and height to exactly half camera.resolution. Anything else may result in cropping and noisy images.
+		self.width = 960				# Video file horizontal resolution
+		self.height = 720				# Video file vertical resolution
+		self.framerate = 15				# Video file framerate.
+		self.minimumLength = 10				# Minimum duration of a recording after motion stops. Lower number results in more files, higher number results in fewer, longer files.
+		self.rotation = 0				# Rotates image (warning: cropping will occur!)
 		self.filepath = "/home/leo/camera/"		# Local file path for video files
-		self.prefix = ""						# Optional filename prefix
-		self.testInterval = 0.5					# Interval at which stills are captured to test for motion
-		self.tWidth = 96						# motion testing horizontal resolution. Use low values!
-		self.tHeight = 72						# motion testing vertical resolution. Use low values!
-		self.threshold = 15						# How much a pixel value has to change to consider it "motion"
-		self.sensitivity = 25					# How many pixels have to change to trigger motion detection
+		self.prefix = ""				# Optional filename prefix
+		self.testInterval = 0.5				# Interval at which stills are captured to test for motion
+		self.tWidth = 96				# motion testing horizontal resolution. Use low values!
+		self.tHeight = 72				# motion testing vertical resolution. Use low values!
+		self.threshold = 15				# How much a pixel value has to change to consider it "motion"
+		self.sensitivity = 25				# How many pixels have to change to trigger motion detection
 		
-		self.convertToMp4 = True				# Requires GPAC to be installed. Removes original .h264 file
+		self.convertToMp4 = False			# Requires GPAC to be installed. Removes original .h264 file
 		self.useDateAsFolders = True			# Creates folders with current year, month and day, then saves file in the day folder.
 
 		self.camera = picamera.PiCamera()		# The camera object
 		self.timeWithoutActivity = 0.0			# How long since last motion detection
-		self.lastActiveTime = 0.0				# The time at which the last motion detection occurred
-		self.isRecording = False				# Is the camera currently recording? Prevents stopping a camera that is not recording.
-		self.skip = True						# Skips the first frame, to prevent a false positive motion detection (since the first image1 is black)
+		self.lastActiveTime = 0.0			# The time at which the last motion detection occurred
+		self.isRecording = False			# Is the camera currently recording? Prevents stopping a camera that is not recording.
+		self.skip = True				# Skips the first frame, to prevent a false positive motion detection (since the first image1 is black)
 		self.filename = ""
 		self.mp4name = ""
 		self.folderPath = ""
 
 		self.camera.resolution = ( 1920, 1440 )	# Set this to a high resolution, scale the video file down using "width" and "height" to reduce noise.
-												# Setting this to more than 1920x1440 seems to cause slowdown (records at a lower framerate)
-												# To achieve 30 fps, set this to 1920x1080 or less (cropping will occur)
+							# Setting this to more than 1920x1440 seems to cause slowdown (records at a lower framerate)
+							# To achieve 30 fps, set this to 1920x1080 or less (cropping will occur)
 												
 		self.camera.framerate = self.framerate	# Sets camera framerate
 		self.camera.rotation = self.rotation	# Sets camera rotation
 	
 		self.image1 = Image.new( 'RGB', (self.tWidth, self.tHeight) )	# initializes image1
 		self.image2 = Image.new( 'RGB', (self.tWidth, self.tHeight) )	# initializes image2
-		self.buffer1 = self.image1.load()								# initializes image1 "raw data" buffer
-		self.buffer2 = self.image2.load()								# initializes image2 "raw data" buffer
+		self.buffer1 = self.image1.load()				# initializes image1 "raw data" buffer
+		self.buffer2 = self.image2.load()				# initializes image2 "raw data" buffer
 																		# The difference here is that image1 is handled like a file stream, while the buffer is the actual RGB byte data, if I understand it correctly!
 	
 	def StartRecording( self ):
