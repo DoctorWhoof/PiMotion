a75b151bb5d9d27297c77a7312bfca372e4e17b7
DoctorWhoof <contact@leosantos.com>
2014-05-07 23:47:24 -0700

    New features
    Added date folder up support ( year/month/day/file.h264) and convert to
    .mp4 option (requires Gpac).
    Improved default settings for better quality.

--- pimotion.py
+++ pimotion.py
@@ -2,12 +2,15 @@
 # Script created by Leo Santos ( http://www.leosantos.com )
 # Based on the picam.py script (by brainflakes, pageauc, peewee2 and Kesthal) and picamera examples.
 # Dependencies: PIL and picamera. Run "sudo apt-get install python-imaging-tk" and "sudo apt-get install python-picamera" to get those.
+# If you want .mp4 files instead of .h264, install gpac with "sudo apt-get install -y gpac" and set convertToMp4=True
 
 import picamera
 import cStringIO
 import os
 import sys
 import time
+import subprocess
+import errno
 
 from datetime import datetime
 from PIL import Image
@@ -15,49 +18,72 @@ from PIL import Image
 class Motion:
 
 	def __init__( self ):
-		self.width = 1024				# Video file horizontal resolution
-		self.height = 768				# Video file vertical resolution
-		self.framerate = 15				# Video file framerate. For "realtime" playback, set to 30.
-		self.minimumLength = 5.				# Minimum duration of a recording after motion stops. Lower number results in more files, higher number results in fewer, longer files.
-		self.rotation = 0				# Rotates image (warning: cropping will occur!)
-		self.filepath = "/home/"			# Local file path for video files
-		self.prefix = ""				# Optional filename prefix
-		self.testInterval = 0.5				# Interval at which stills are captured to test for motion
-		self.tWidth = 96				# motion testing horizontal resolution. Use low values!
-		self.tHeight = 72				# motion testing vertical resolution. Use low values!
-		self.threshold = 15				# How much a pixel value has to change to consider it "motion"
-		self.sensitivity = 25				# How many pixels have to change to trigger motion detection
+												# For best results (by far) set width and height to exactly half camera.resolution. Anything else may result in cropping and noisy images.
+		self.width = 960						# Video file horizontal resolution
+		self.height = 720						# Video file vertical resolution
+		self.framerate = 15						# Video file framerate.
+		self.minimumLength = 10					# Minimum duration of a recording after motion stops. Lower number results in more files, higher number results in fewer, longer files.
+		self.rotation = 0						# Rotates image (warning: cropping will occur!)
+		self.filepath = "/home/leo/camera/"		# Local file path for video files
+		self.prefix = ""						# Optional filename prefix
+		self.testInterval = 0.5					# Interval at which stills are captured to test for motion
+		self.tWidth = 96						# motion testing horizontal resolution. Use low values!
+		self.tHeight = 72						# motion testing vertical resolution. Use low values!
+		self.threshold = 15						# How much a pixel value has to change to consider it "motion"
+		self.sensitivity = 25					# How many pixels have to change to trigger motion detection
+		
+		self.convertToMp4 = True				# Requires GPAC to be installed. Removes original .h264 file
+		self.useDateAsFolders = True			# Creates folders with current year, month and day, then saves file in the day folder.
 
 		self.camera = picamera.PiCamera()		# The camera object
 		self.timeWithoutActivity = 0.0			# How long since last motion detection
-		self.lastActiveTime = 0.0			# The time at which the last motion detection occurred
-		self.isRecording = False			# Is the camera currently recording? Prevents stopping a camera that is not recording.
-		self.skip = True				# Skips the first frame, to prevent a false positive motion detection (since the first image1 is black)
+		self.lastActiveTime = 0.0				# The time at which the last motion detection occurred
+		self.isRecording = False				# Is the camera currently recording? Prevents stopping a camera that is not recording.
+		self.skip = True						# Skips the first frame, to prevent a false positive motion detection (since the first image1 is black)
+		self.filename = ""
+		self.mp4name = ""
+		self.folderPath = ""
 
-		self.camera.resolution = ( 2592, 1944 )		# Records always at max resolution, scales down before saving to reduce noise.
-		self.camera.framerate = self.framerate		# Sets camera framerate
-		self.camera.rotation = self.rotation		# Sets camera rotation
+		self.camera.resolution = ( 1920, 1440 )	# Set this to a high resolution, scale the video file down using "width" and "height" to reduce noise.
+												# Setting this to more than 1920x1440 seems to cause slowdown (records at a lower framerate)
+												# To achieve 30 fps, set this to 1920x1080 or less (cropping will occur)
+												
+		self.camera.framerate = self.framerate	# Sets camera framerate
+		self.camera.rotation = self.rotation	# Sets camera rotation
 	
 		self.image1 = Image.new( 'RGB', (self.tWidth, self.tHeight) )	# initializes image1
 		self.image2 = Image.new( 'RGB', (self.tWidth, self.tHeight) )	# initializes image2
-		self.buffer1 = self.image1.load()				# initializes image1 "raw data" buffer
-		self.buffer2 = self.image2.load()				# initializes image2 "raw data" buffer
+		self.buffer1 = self.image1.load()								# initializes image1 "raw data" buffer
+		self.buffer2 = self.image2.load()								# initializes image2 "raw data" buffer
 																		# The difference here is that image1 is handled like a file stream, while the buffer is the actual RGB byte data, if I understand it correctly!
 	
 	def StartRecording( self ):
 		if not self.isRecording and not self.skip:
 			timenow = datetime.now()
-			filename = self.filepath + self.prefix + "%04d%02d%02d-%02d%02d%02d.h264" % ( timenow.year, timenow.month, timenow.day, timenow.hour, timenow.minute, timenow.second )
-			self.camera.start_recording( filename, resize=( self.width, self.height) )
+			
+			if self.useDateAsFolders:
+				self.folderPath = self.filepath +"%04d/%02d/%02d" % ( timenow.year, timenow.month, timenow.day )
+				subprocess.call( [ "mkdir", "-p", self.folderPath ] )
+				self.filename = self.folderPath + "/" + self.prefix + "%02d%02d%02d.h264" % ( timenow.hour, timenow.minute, timenow.second )
+			else:
+				self.filename = self.filepath + self.prefix + "%04d%02d%02d-%02d%02d%02d.h264" % ( timenow.year, timenow.month, timenow.day, timenow.hour, timenow.minute, timenow.second )
+
+			self.mp4name = self.filename[:-4] + "mp4"
+			self.camera.start_recording( self.filename, resize=( self.width, self.height) )
 			self.isRecording = True
 			self.lastActiveTime = time.clock()
-			print "Started recording %s" % filename
+			print "Started recording %s" % self.filename
 
 	def StopRecording( self ):
 		if self.isRecording and ( self.timeWithoutActivity > self.minimumLength ):
 			self.camera.stop_recording()
 			self.isRecording = False
-			print "Stopped recording."
+			if self.convertToMp4:
+				subprocess.call( [ "MP4Box","-fps",str(self.framerate),"-add",self.filename,self.mp4name ] )
+				subprocess.call( [ "rm", self.filename ] )
+				print "\n"
+			else:
+				print "Finished recording."
 
 	def CaptureTestImage( self ):
 		self.image1 = self.image2
@@ -87,11 +113,12 @@ class Motion:
 
 motion = Motion()
 print "Warming up camera..."
-time.sleep( 2 )
+time.sleep( 1 )
 print "Camera ready to record. Use Ctrl+C to stop."
 
 try:
 	while True:
+		motion.camera.start_preview()
 		if motion.TestMotion():
 			motion.StartRecording()
 		else:
@@ -102,5 +129,6 @@ except KeyboardInterrupt:
 		print "\nClosing camera. Bye!"
 		if motion.isRecording:
 			motion.camera.stop_recording()
+		motion.camera.stop_preview()
 		motion.camera.close()
 		sys.exit(1)
